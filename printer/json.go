a51b9ae2a9eee4e98af5570e1de0368ca89056f0
Hidetatsu Yaginuma <deetyler@protonmail.com>
2020-09-26 23:41:35 +0900

    add json printer

---
+++ printer/json.go
@@ -0,0 +1,32 @@
+package printer
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/hokaccha/go-prettyjson"
+
+	fcolor "github.com/fatih/color"
+)
+
+func printJson(w io.Writer, output []byte) error {
+	// TODO Need to decide color policy
+	f := &prettyjson.Formatter{
+		KeyColor:        fcolor.New(fcolor.Attribute(KeyColor.Sequence())),
+		StringColor:     fcolor.New(fcolor.Attribute(StringColor.Sequence())),
+		BoolColor:       fcolor.New(fcolor.Attribute(BoolColor.Sequence())),
+		NumberColor:     fcolor.New(fcolor.Attribute(NumberColor.Sequence())),
+		NullColor:       fcolor.New(fcolor.Attribute(NullColor.Sequence())),
+		StringMaxLength: 0,
+		DisabledColor:   false,
+		Indent:          2,
+		Newline:         "\n",
+	}
+	colorized, err := f.Format(output)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, string(colorized))
+	return nil
+}
