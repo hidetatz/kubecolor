177a19bdf3d18a896a151f0c28ca5c4128f7ad27
Hidetatsu Yaginuma <deetyler@protonmail.com>
2020-10-05 18:21:42 +0900

    use stdout/err explicitly

--- command/runner.go
+++ command/runner.go
@@ -1,9 +1,11 @@
 package command
 
 import (
-	"context"
-	"fmt"
+	"bytes"
+	"io"
 	"os"
+	"os/exec"
+	"sync"
 
 	"github.com/dty1er/kubecolor/color"
 	"github.com/dty1er/kubecolor/kubectl"
@@ -11,20 +13,63 @@ import (
 )
 
 func Run(args []string) error {
-	ctx := context.Background()
-	output, err := kubectl.Execute(ctx, args)
+	cmd := exec.Command("kubectl", args...)
 
+	outReader, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, color.Apply(err.Error(), color.Red))
-		return nil
+		return err
 	}
 
-	subcommandInfo, err := kubectl.InspectSubcommandInfo(args)
+	errReader, err := cmd.StderrPipe()
 	if err != nil {
-		printer.PrintPlain(output)
-		return nil
+		return err
 	}
 
-	printer.Print(output, subcommandInfo)
+	var bufout, buferr bytes.Buffer
+	bufoutReader := io.TeeReader(outReader, &bufout)
+	buferrReader := io.TeeReader(errReader, &buferr)
+
+	if err = cmd.Start(); err != nil {
+		return err
+	}
+
+	subcommandInfo, ok := kubectl.InspectSubcommandInfo(args)
+
+	wg := &sync.WaitGroup{}
+
+	switch {
+	case subcommandInfo.Help:
+		runAsync(wg, []func(){
+			func() { printer.PrintWithColor(bufoutReader, os.Stdout, color.Yellow) },
+			func() { printer.PrintErrorOrWarning(buferrReader, os.Stderr) },
+		})
+	case !ok:
+		// given subcommand is not supported to colorize
+		// so just print it in green
+		runAsync(wg, []func(){
+			func() { printer.PrintWithColor(bufoutReader, os.Stdout, color.Green) },
+			func() { printer.PrintErrorOrWarning(buferrReader, os.Stderr) },
+		})
+	default:
+		runAsync(wg, []func(){
+			func() { printer.Print(bufoutReader, os.Stdout, subcommandInfo) },
+			func() { printer.PrintErrorOrWarning(buferrReader, os.Stderr) },
+		})
+	}
+
+	cmd.Wait()
+	wg.Wait()
+
 	return nil
+}
+
+func runAsync(wg *sync.WaitGroup, tasks []func()) {
+	wg.Add(len(tasks))
+	for _, task := range tasks {
+		task := task
+		go func() {
+			task()
+			wg.Done()
+		}()
+	}
 }
