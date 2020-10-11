10fb8d853eb3dde985c4448b154cd49be7ef4f20
Hidetatsu Yaginuma <deetyler@protonmail.com>
2020-10-11 20:33:14 +0900

    fix to use exported var directly

--- printer/printer.go
+++ printer/printer.go
@@ -29,22 +29,16 @@ var tab = regexp.MustCompile("\\s{2,}")
 
 func Print(outReader io.Reader, writer io.Writer, subcommandInfo *kubectl.SubcommandInfo) {
 	withHeader := !subcommandInfo.NoHeader
-	switch subcommandInfo.Subcommand.String() {
-	case "top":
-		// kubectl top supports only pod or node
-		// if subcommandInfo.Target != kubectl.Pod && subcommandInfo.Target != kubectl.Node {
-		// 	PrintPlain(outReader, writer)
-		// 	return
-		// }
-
+	switch subcommandInfo.Subcommand {
+	case kubectl.Top:
 		printer := &TopPrinter{Writer: writer, WithHeader: withHeader}
 		printer.Print(outReader)
 
-	case "get":
+	case kubectl.Get:
 		printer := &GetPrinter{Writer: writer, WithHeader: withHeader, FormatOpt: subcommandInfo.FormatOption}
 		printer.Print(outReader)
 
-	case "describe":
+	case kubectl.Describe:
 		printer := &DescribePrinter{Writer: writer}
 		printer.Print(outReader)
 
