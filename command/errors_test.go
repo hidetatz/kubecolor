17fc6ab66490ff9ce8073ae14a5da6837e171215
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-02-09 12:28:53 +0900

    fix test

--- command/errors_test.go
+++ command/errors_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestKubectlError(t *testing.T) {
 	e := &KubectlError{1}
-	if e.Error() != "kubectl error 1" {
+	if e.Error() != "kubectl error: 1" {
 		t.Errorf("unexpected error")
 	}
 }
